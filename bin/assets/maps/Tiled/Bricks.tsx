98a880e8f9745888682797e81fd93f51be0d67e1
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2017-11-13 23:17:56 +0100

    Initial commit with gitignore, gitmodules and assets
    Change-Id: I25d1b9e080cd46388699ca029a8ffb0fc4b261bc

---
+++ bin/assets/maps/Tiled/Bricks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Bricks" tilewidth="148" tileheight="164" tilecount="12" columns="4">
+ <image source="Bricks.png" width="592" height="492"/>
+</tileset>
