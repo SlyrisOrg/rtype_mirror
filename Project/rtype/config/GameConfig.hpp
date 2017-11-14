ae208c58891d0dc65c83aea9d9e1b5060e31c7a4
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2017-11-14 08:44:36 +0100

    Add the Configuration part of RType
    - Each Scenes have is JSON Configuration Files and own cpp/hpp Configuration files for default configuration in case if we can't load the json files.
    - 85% exception safety
    - Based on readability, and user-friendy
    
    Change-Id: I303280fea622e8d104e1b1f654441ec56fd9dea3

---
+++ Project/rtype/config/GameConfig.hpp
@@ -0,0 +1,19 @@
+//
+// Created by milerius on 06/11/17.
+//
+
+#ifndef RTYPE_GAMECONFIG_HPP
+#define RTYPE_GAMECONFIG_HPP
+
+#include <string>
+
+namespace cfg::game
+{
+    extern unsigned int width;
+    extern unsigned int height;
+    extern std::string lang;
+    extern std::string apiURL;
+    extern std::string tokenSession;
+}
+
+#endif //RTYPE_GAMECONFIG_HPP
