33afc115f3ea6ac5b6d71c3b4186421ffd6e87b1
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2017-11-14 08:07:35 +0100

    add main.cpp
    Change-Id: I88e2f062d6fcdfc10bec4a340b2af762c252f119

---
+++ Project/main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by roman sztergbaum on 15/09/2017.
+//
+
+#include <iostream>
+#include <rtype/config/Configuration.hpp>
+#include <rtype/core/Game.hpp>
+
+int main()
+{
+    try {
+        rtype::Configuration::initialize();
+        rtype::Game rtype;
+
+        rtype.run();
+    }
+    catch (const std::exception &error) {
+        std::cerr << error.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
