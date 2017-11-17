6b316d414bbc2974d395547af9edd2002a335694
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2017-11-17 20:10:04 +0100

    Add Core of RType : - 85% safety - System Based - 60 Fps
    Change-Id: Iab05709df2be9f3fb2e94d03707ea9f7a348c64b

---
+++ Project/rtype/core/Game.hpp
@@ -0,0 +1,39 @@
+//
+// Created by roman sztergbaum on 16/09/2017.
+//
+
+#ifndef RTYPE_GAME_HPP
+#define RTYPE_GAME_HPP
+
+#include <rtype/gutils/manager/EventManager.hpp>
+#include <rtype/gutils/event/InsideEvents.hpp>
+#include <rtype/core/World.hpp>
+
+namespace evt = gutils::evt;
+
+namespace rtype
+{
+    class Game final : public World, public gutils::Receiver<Game>
+    {
+    public:
+        Game() noexcept;
+        ~Game() noexcept final;
+
+    public:
+        void receive(const evt::GameShutdown &evt) noexcept;
+        void run();
+
+    private:
+        void __initSystems();
+        void __launch() noexcept;
+        void __update(const sf::Time &time) noexcept;
+        void __sysUpdate(const sf::Time &time) noexcept;
+        void __sysDraw() noexcept;
+
+    private:
+        bool _isRunning{false};
+        logging::Logger _log{"RType", logging::Info};
+    };
+}
+
+#endif //RTYPE_GAME_HPP
