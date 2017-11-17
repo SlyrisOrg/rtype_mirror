9e1e39d3fc968d5395673e029e27db7f92306932
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2017-11-17 20:10:04 +0100

    Add Core of RType : - 85% safety - System Based - 60 Fps
    Change-Id: Iab05709df2be9f3fb2e94d03707ea9f7a348c64b

---
+++ Project/rtype/core/Game.cpp
@@ -0,0 +1,86 @@
+//
+// Created by roman sztergbaum on 16/09/2017.
+//
+
+#include <SFML/Graphics.hpp>
+#include <rtype/core/Game.hpp>
+#include <rtype/systems/Systems.hpp>
+
+namespace lg = logging;
+
+namespace rtype
+{
+    Game::Game() noexcept
+    {
+        _evtMgr.subscribe<evt::GameShutdown>(*this);
+    }
+
+    Game::~Game() noexcept
+    {
+        _log(lg::Info) << "Shutting down RType." << std::endl;
+    }
+
+    //! Private member functions
+    void Game::__initSystems()
+    {
+        _systemMgr.add<SEvents>(_win);
+        _systemMgr.add<SGui>(_win);
+        _systemMgr.add<SScenes>(_win);
+        _systemMgr.add<SVideo>(_win);
+        _systemMgr.add<SAudio>();
+        _log(lg::Info) << "Successfully initialized RType" << std::endl;
+        _evtMgr.emit<evt::EnterScene>(Scene::Intro);
+    }
+
+    void Game::__sysUpdate(const sf::Time &time) noexcept
+    {
+        _systemMgr[Sys::Gui].update(time.asSeconds());
+        _systemMgr[Sys::Audio].update(time.asSeconds());
+        _systemMgr[Sys::Video].update(time.asSeconds());
+        _systemMgr[Sys::Scene].update(time.asSeconds());
+    }
+
+    void Game::__sysDraw() noexcept
+    {
+        _systemMgr[Sys::Video].draw();
+        _systemMgr[Sys::Scene].draw();
+        _systemMgr[Sys::Gui].draw();
+    }
+
+    void Game::__update(const sf::Time &time) noexcept
+    {
+        __sysUpdate(time);
+        _win.clear();
+        __sysDraw();
+        _win.display();
+    }
+
+    void Game::__launch() noexcept
+    {
+        sf::Clock clock;
+        sf::Time timeSinceLastUpdate;
+        sf::Time TimePerFrame = sf::seconds(1.f / 60.f);
+
+        while (!_isRunning) {
+            _systemMgr[Sys::Event].update(TimePerFrame.asSeconds());
+            timeSinceLastUpdate = clock.restart();
+            while (timeSinceLastUpdate > TimePerFrame) {
+                timeSinceLastUpdate -= TimePerFrame;
+                __update(TimePerFrame);
+            }
+            __update(timeSinceLastUpdate);
+        }
+    }
+
+    //! Public member functions
+    void Game::receive([[maybe_unused]] const evt::GameShutdown &evt) noexcept
+    {
+        _isRunning = true;
+    }
+
+    void Game::run()
+    {
+        __initSystems();
+        __launch();
+    }
+}
