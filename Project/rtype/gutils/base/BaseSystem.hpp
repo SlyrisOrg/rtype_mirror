d9b4029fcbc6a5d093b199e5318e354ac0c46174
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2017-12-15 20:24:22 +0100

    Add game utils part of RType: - An EventManager - An SystemManager - Common game events
    Change-Id: If5a15403685aeb9ebbfe468a8c17e31fe0969560

---
+++ Project/rtype/gutils/base/BaseSystem.hpp
@@ -0,0 +1,58 @@
+//
+// Created by roman sztergbaum on 16/09/2017.
+//
+
+#ifndef GUTILS_ISYSTEM_HPP
+#define GUTILS_ISYSTEM_HPP
+
+#include <string_view>
+#include <rtype/gutils/manager/EventManager.hpp>
+#include <utils/NonCopyable.hpp>
+
+namespace gutils
+{
+    class BaseSystem : private utils::NonCopyable
+    {
+    public:
+        explicit BaseSystem(gutils::EventManager &evtMgr) noexcept : _evtMgr(evtMgr)
+        {
+        }
+
+        ~BaseSystem() noexcept override = default;
+
+    public:
+        //! update the current system
+        virtual void update(double deltaTime) noexcept = 0;
+
+        //! Window / Gui / Scene System can draw
+        virtual void draw() noexcept {}
+
+    protected:
+        gutils::EventManager &_evtMgr;
+    };
+
+    template <typename Derived>
+    class System : public BaseSystem
+    {
+    public:
+        System(gutils::EventManager &evtMgr, std::string systemName) noexcept : BaseSystem(evtMgr),
+                                                                                _systemName(std::move(systemName))
+        {
+            _log(logging::Debug) << "System " << _systemName << " successfully initialized." << std::endl;
+        }
+
+        ~System() noexcept override
+        {
+            _log(logging::Debug) << "System " << _systemName << " shutdown." << std::endl;
+        }
+
+    private:
+        friend class SystemManager;
+
+    protected:
+        std::string _systemName;
+        logging::Logger _log{_systemName, logging::Info};
+    };
+}
+
+#endif //GUTILS_ISYSTEM_HPP
