4156a946014e4a8738e22369d2413292062fc370
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2017-12-15 18:48:45 +0100

    Add game utils part of RType: - An EventManager - An SystemManager - Common game events
    Change-Id: I38cbd20815fb202f3a8a8d128b8521dfb5b5218e

--- Project/rtype/gutils/CMakeSources.cmake
+++ Project/rtype/gutils/CMakeSources.cmake
@@ -4,6 +4,6 @@ set(RTYPE_GUTILS_HEADERS
         ${GUTILS}/manager/EventManager.hpp
         ${GUTILS}/manager/SystemManager.hpp
         ${GUTILS}/base/AScene.hpp
-        ${GUTILS}/base/ISystem.hpp
+        ${GUTILS}/base/BaseSystem.hpp
         ${GUTILS}/event/InsideEvents.hpp
         PARENT_SCOPE)
