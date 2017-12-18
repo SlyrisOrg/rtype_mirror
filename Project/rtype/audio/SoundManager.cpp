29c20683295aba24583eb1df70d2922b582c21db
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2017-12-18 02:09:28 +0100

    Update audio part of RType: - add new callback - clear the code
    Change-Id: I62cc1e4210aaba92674716f0354e8b2894aef017

--- Project/rtype/audio/SoundManager.cpp
+++ Project/rtype/audio/SoundManager.cpp
@@ -11,6 +11,7 @@ namespace rtype
         _evtMgr.subscribe<evt::PlayMusic>(*this);
         _evtMgr.subscribe<evt::StopMusic>(*this);
         _evtMgr.subscribe<evt::PlaySoundEffect>(*this);
+        _evtMgr.subscribe<evt::ResetMusicVolume>(*this);
     }
 
     //! Public Member functions.
@@ -24,27 +25,31 @@ namespace rtype
     //! Callbacks
     void SoundManager::receive(const gutils::evt::PlaySoundEffect &evt) noexcept
     {
-        sf::Sound sound;
-        _sndQueue.push_back(sound);
-        auto &snd = _sndQueue.back();
-        snd.setBuffer(evt.buff);
-        snd.setVolume(evt.volume);
-        snd.setLoop(evt.loop);
-        snd.play();
-        _log(lg::Info) << "Playing Sound Effect : " << evt.soundEffectName << ".ogg" << std::endl;
+        auto &sound = _sndQueue.emplace_back();
+        sound.setBuffer(evt.buff);
+        sound.setVolume(cfg::game::soundseffectVolume);
+        sound.setLoop(evt.loop);
+        sound.play();
+        _log(lg::Info) << "Playing Sound Effect: " << evt.soundEffectName << ".ogg" << std::endl;
     }
 
     void SoundManager::receive(const evt::StopMusic &evt) noexcept
     {
         evt.music.stop();
-        _log(lg::Info) << "Stopping music : " << evt.musicName << ".ogg" << std::endl;
+        _log(lg::Info) << "Stopping music: " << evt.musicName << ".ogg" << std::endl;
+    }
+
+    void SoundManager::receive([[maybe_unused]] const evt::ResetMusicVolume &evt) noexcept
+    {
+        evt.music.setVolume(cfg::game::musicVolume);
+        _log(lg::Info) << "Reset volume music: " << evt.musicName << ".ogg" << std::endl;
     }
 
     void SoundManager::receive(const evt::PlayMusic &evt) noexcept
     {
-        evt.music.setVolume(evt.volume);
+        evt.music.setVolume(cfg::game::musicVolume);
         evt.music.setLoop(evt.loop);
         evt.music.play();
-        _log(lg::Info) << "Playing music : " << evt.musicName << ".ogg" << std::endl;
+        _log(lg::Info) << "Playing music: " << evt.musicName << ".ogg" << std::endl;
     }
 }
