a4772e4ac5b6dcc97b60a9f65a34f8be977efa68
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2018-01-12 12:03:06 +0100

    add AnimationSystem class in demo namespace, getCurrentFrame for animated sprite, and a component movement, enum Direction
    Change-Id: I042e2a070711b1e5dc9fce08b0d80dacb20d8ded

--- Project/rtype/scenes/DemoScene.hpp
+++ Project/rtype/scenes/DemoScene.hpp
@@ -16,9 +16,8 @@
 
 namespace rtype
 {
-    class DemoScene final : public gutils::AScene, public rtype::ActionTarget<int>
+    namespace demo
     {
-    public:
         ENUM(Sprite,
              BheetLv1AttackTopDown,
              BheetLv1AttackTurnUp,
@@ -30,15 +29,147 @@ namespace rtype
              Bullet
         );
 
-        ENUM(Animation,
-             BheetLv1AttackTopDown,
-             BheetLv1AttackTurnUp,
-             BheetLv1AttackTurnDown,
-             BheetLv1AttackRedressUp,
-             BheetLv1AttackRedressDown,
-             BheetLv1AttackRollUp,
-             BheetLv1AttackRollDown
-        );
+        using SpriteT = demo::Sprite::EnumType;
+
+        class AnimationSystem
+        {
+        public:
+            ENUM(Animation,
+                 BheetLv1AttackTopDown,
+                 BheetLv1AttackTurnUp,
+                 BheetLv1AttackTurnDown,
+                 BheetLv1AttackRedressUp,
+                 BheetLv1AttackRedressDown,
+                 BheetLv1AttackRollUp,
+                 BheetLv1AttackRollDown
+            );
+
+        private:
+            using AnimT = Animation::EnumType;
+
+        public:
+            AnimationSystem(sf::RenderWindow &win, EntityManager &ettMgr, bool &debugMode,
+                            sfutils::ResourceManager<sf::Texture, demo::SpriteT> &res) noexcept :
+                _win(win), _ettMgr(ettMgr), _debugMode(debugMode), _textures(res)
+            {
+            }
+
+            void clear() noexcept
+            {
+                _animations.clear();
+            }
+
+            sfutils::Animation *get(AnimT anim) const
+            {
+                return &_animations.get(anim);
+            }
+
+            void update(double timeSinceLastFrame) noexcept
+            {
+                sf::Time time = sf::seconds(static_cast<float>(timeSinceLastFrame));
+                _ettMgr.for_each<rtc::Animation, rtc::BoundingBox>([&time](rtype::Entity &ett) {
+                    sfutils::AnimatedSprite &anim = ett.getComponent<rtc::Animation>().anim;
+                    anim.update(time);
+                });
+                auto[movement, animation] = _ettMgr[_playerID].getComponents<rtc::Movement, rtc::Animation>();
+                if (animation.anim.getStatus() == sfutils::AnimatedSprite::Stopped &&
+                    (static_cast<AnimT>(animation.currentAnim) == Animation::BheetLv1AttackRedressUp ||
+                     (static_cast<AnimT>(animation.currentAnim) == Animation::BheetLv1AttackRedressDown))) {
+                    movement.dir = rtc::Direction::None;
+                    animation.anim.setAnimation(&_animations.get(Animation::BheetLv1AttackTopDown));
+                    animation.currentAnim = Animation::BheetLv1AttackTopDown;
+                }
+            }
+
+            void draw() noexcept
+            {
+                _ettMgr.for_each<rtc::Animation>([this](rtype::Entity &ett) {
+                    if (this->_debugMode)
+                        _win.draw(ett.getComponent<rtc::BoundingBox>().shapeDebug);
+                    _win.draw(ett.getComponent<rtc::Animation>().anim);
+                });
+            }
+
+            void loadAnimation(const Sprite &sprite, const Animation &anim,
+                               [[maybe_unused]] sf::Vector2<unsigned int> &&sprInfo,
+                               sf::Vector3<unsigned int> &&sheetInfo)
+            {
+                __initFrames(anim, sprite, sheetInfo.y, sheetInfo.x, sheetInfo.z);
+            }
+
+            void setPlayerID(Entity::ID playerID) noexcept
+            {
+                _playerID = playerID;
+            }
+
+            void changePlayerAnim() noexcept
+            {
+                auto[movement, animation, player] = _ettMgr[_playerID].getComponents<rtc::Movement, rtc::Animation,
+                    rtc::Player>();
+                rtc::Direction dir = movement.dir;
+                auto curAnim = static_cast<AnimT>(animation.currentAnim);
+                size_t currentFrame = animation.anim.getCurrentFrame();
+
+                auto animChange = [this, &currentFrame, &animation](int currentAnim) {
+                    animation.currentAnim = currentAnim;
+                    animation.anim.setAnimation(&_animations.get(static_cast<AnimT>(animation.currentAnim)),
+                                                animation.anim.getAnimation()->size() - currentFrame);
+                    animation.anim.play();
+                };
+                switch (dir) {
+                    case rtc::Direction::None:
+                        if (curAnim == Animation::BheetLv1AttackTurnUp) {
+                            _log(lg::Debug) << "Redress up" << std::endl;
+                            animChange(player.map[curAnim].second);
+                        } else if (curAnim == Animation::BheetLv1AttackTurnDown) {
+                            _log(lg::Debug) << "Redress Down" << std::endl;
+                            animChange(player.map[curAnim].first);
+                        }
+                        break;
+                    case rtc::Direction::North:
+                        animChange(player.map[curAnim].first);
+                        break;
+                    case rtc::Direction::South:
+                        animChange(player.map[curAnim].second);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        private:
+            void __initFrames(const Animation &anim,
+                              const Sprite &sprite,
+                              unsigned int nbColumns,
+                              unsigned int nbLines,
+                              unsigned int nbAnims)
+            {
+                auto &text = _animations.load(anim, &_textures.get(sprite));
+                auto end = nbLines - 1;
+                for (unsigned int i = 0; i < end; i++) {
+                    text.addFramesLine(nbColumns, nbLines, i, nbColumns);
+                }
+                auto animLastLine = (nbAnims - ((nbLines - 1) * nbColumns));
+                text.addFramesLine(animLastLine, nbLines, nbLines - 1, nbColumns);
+                _log(lg::Debug) << "Animation: " << anim.toString() << " information : nbColumns -> " << nbColumns
+                                << ", nbLines" << nbLines << ", nbAnims: " << nbAnims << std::endl;
+                _log(lg::Debug) << "Loading Animation " << anim.toString() << " successfully loaded." << std::endl;
+            }
+
+        private:
+            sf::RenderWindow &_win;
+            EntityManager &_ettMgr;
+            bool &_debugMode;
+            logging::Logger _log{"DemoAnimationSystem", logging::Debug};
+            sfutils::ResourceManager<sf::Texture, SpriteT> &_textures;
+            sfutils::ResourceManager<sfutils::Animation, AnimT> _animations;
+            Entity::ID _playerID;
+        };
+    }
+
+    class DemoScene final : public gutils::AScene, public rtype::ActionTarget<int>
+    {
+    public:
 
     public:
         template <typename ...Args>
@@ -64,36 +195,26 @@ namespace rtype
 
     private:
         using UIGUI = BaseGUI<cfg::play::nbWidgets>;
-        using SpriteDemo = Sprite::EnumType;
-        using AnimDemo = Animation::EnumType;
+        using AnimDemo = demo::AnimationSystem::Animation::EnumType;
 
     private:
         void __configure() noexcept;
         bool __setGUI() noexcept;
         void __parseConfig(const std::string &faction);
         void __parseConfigInside(const rapidjson::Document &doc, const std::string &string);
-        void __loadSprite(const Sprite &val);
-        void __loadAnimation(const Sprite &sprite, const Animation &anim,
-                             sf::Vector2<unsigned int> &&sprInfo,
-                             sf::Vector3<unsigned int> &&sheetInfo);
-        void __initFrames(const Animation &anim,
-                          const Sprite &sprite,
-                          unsigned int nbColumns,
-                          unsigned int nbLines,
-                          unsigned int nbAnims);
+        void __loadSprite(const demo::Sprite &val);
         void __createGameObjects() noexcept;
-        void __animationSystem(const sf::Time &time) noexcept;
         void __bulletSystem(double timeSinceLastFrame) noexcept;
         void __loadBulletSprite() noexcept;
         void __setPlayerCallbacks() noexcept;
         void __unbindPlayerCallbacks() noexcept;
     private:
         UIGUI _gui;
-        sfutils::ResourceManager<sf::Texture, SpriteDemo> _textures;
-        sfutils::ResourceManager<sfutils::Animation, AnimDemo> _animations;
+        sfutils::ResourceManager<sf::Texture, demo::SpriteT> _textures;
         std::unordered_map<std::string, sf::IntRect> _boundingBoxFactions;
-        EntityManager _ettMgr;
         bool _debugMode{false};
+        EntityManager _ettMgr;
+        demo::AnimationSystem _animSystem{_win, _ettMgr, _debugMode, _textures};
         std::chrono::time_point<std::chrono::steady_clock> _lastShoot;
         Entity::ID _playerID;
     };
