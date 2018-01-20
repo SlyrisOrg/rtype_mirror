87b18b317414eeeb7fc11eda5bfdb46e71f941a9
king <arthur.cros@epitech.eu>
2018-01-20 12:04:27 +0100

    add layer system, parallax, tons of assets and working stars nebula and planets
    Change-Id: I51ca952539b8caec2851ae69dffb74306b8a0e34

--- Project/rtype/scenes/DemoScene.hpp
+++ Project/rtype/scenes/DemoScene.hpp
@@ -16,148 +16,13 @@
 #include <rtype/gutils/base/AScene.hpp>
 #include <rtype/utils/Action.hpp>
 #include <rtype/utils/QuadTree.hpp>
+#include <rtype/utils/DemoUtils.hpp>
+#include <rtype/systems/FieldSystem.hpp>
 
 namespace rtype
 {
     namespace demo
     {
-        ENUM(Sprite,
-             BheetLv1AttackTopDown,
-             BheetLv1AttackTurnUp,
-             BheetLv1AttackTurnDown,
-             BheetLv1AttackRedressUp,
-             BheetLv1AttackRedressDown,
-             BheetLv1AttackRollUp,
-             BheetLv1AttackRollDown,
-             Bullet,
-             Flare01,
-             Flare02,
-             Flare03,
-             Flare04);
-
-        using SpriteT = demo::Sprite::EnumType;
-
-        class StarfieldSystem
-        {
-        public:
-            StarfieldSystem(sf::RenderWindow &win, EntityManager &ettMgr,
-                            sfutils::ResourceManager<sf::Texture, SpriteT> &textures,
-                            gutils::EventManager &evtMgr) noexcept :
-                _win(win),
-                _ettMgr(ettMgr),
-                _textures(textures),
-                _evtMgr(evtMgr)
-            {
-            };
-
-        public:
-            struct Star
-            {
-                unsigned int nbStars;
-                float scale;
-                demo::SpriteT sprite;
-                //unsigned char alpha;
-            };
-
-            std::vector<Star> __parseConfigInside(const rapidjson::Document &document) noexcept
-            {
-                std::vector<Star> stars;
-                const auto &starfieldCFG = document["starfield"];
-                const auto &speedCFG = starfieldCFG.GetObject()["speedDistribution"];
-                const auto &posXCFG = starfieldCFG.GetObject()["posXDistribution"];
-                const auto &posYCFG = starfieldCFG.GetObject()["posYDistribution"];
-                const auto &starCFG = starfieldCFG.GetObject()["stars"];
-                _speedDistribution.param(std::uniform_real_distribution<float>(speedCFG.GetArray()[0].GetFloat(),
-                                                                               speedCFG.GetArray()[1].GetFloat()).param());
-                _distX.param(std::uniform_real_distribution<float>(posXCFG.GetArray()[0].GetFloat(),
-                                                                   posXCFG.GetArray()[1].GetFloat()).param());
-                _distY.param(std::uniform_real_distribution<float>(posYCFG.GetArray()[0].GetFloat(),
-                                                                   posYCFG.GetArray()[1].GetFloat()).param());
-                for (const auto &v : starCFG.GetArray()) {
-                    Star star;
-                    std::string_view spriteName = v.GetObject()["texture"].GetString();
-                    Sprite spr = spriteName;
-                    star.sprite = spr;
-                    star.nbStars = v.GetObject()["nbStars"].GetUint();
-                    star.scale = v.GetObject()["scale"].GetFloat();
-                    stars.push_back(std::move(star));
-                }
-                return stars;
-            }
-
-            std::vector<Star> __parseConfigStarfield() noexcept
-            {
-                using namespace std::string_literals;
-                try {
-                    auto path = cfg::configPath + "starfield/starfield.json"s;
-                    rapidjson::Document doc;
-                    std::ifstream ifs(path);
-                    rapidjson::IStreamWrapper isw(ifs);
-                    doc.ParseStream(isw);
-                    _log(lg::Info) << "Parsing file: " << path << std::endl;
-                    if (doc.HasParseError()) {
-                        _log(lg::Error) << doc.GetParseError() << " " << "Offset : " << doc.GetErrorOffset()
-                                        << std::endl;
-                        throw std::runtime_error("Parse file -> "s + path + " failed."s);
-                    }
-                    return __parseConfigInside(doc);
-                }
-                catch (const std::exception &error) {
-                    _log(lg::Error) << error.what() << std::endl;
-                    _evtMgr.emit<gutils::evt::GameShutdown>();
-                }
-                return {};
-            }
-
-            void configure() noexcept
-            {
-                std::vector<Star> stars = __parseConfigStarfield();
-                float speed{0.0f};
-
-                for (auto[nbStars, scale, sprite] : stars) {
-                    for (unsigned int i = 0; i < nbStars; ++i) {
-                        speed = this->_speedDistribution(this->_mt);
-                        GameFactory::createStar(_textures.get(sprite), sf::Vector2f(_distX(_mt), _distY(_mt)),
-                                                speed, scale, true, static_cast<unsigned int>(sprite));
-                    }
-                }
-            }
-
-            void update(double timeSinceLastFrame) noexcept
-            {
-                _ettMgr.for_each<rtc::Star>([this, &timeSinceLastFrame](rtype::Entity &ett) {
-                    sf::Sprite &sprite = ett.getComponent<rtc::Sprite>().sprite;
-                    if (sprite.getPosition().x < 0 && ett.hasComponent<rtc::Speed>()) {
-                        ett.mark();
-                        auto[star, speed, sprite] = ett.getComponents<rtc::Star, rtc::Speed, rtc::Sprite>();
-                        GameFactory::createStar(_textures.get(static_cast<demo::SpriteT>(star.textureID)),
-                                                sf::Vector2f(1920, _distY(_mt)),
-                                                speed.speed, sprite.sprite.getScale().x, true, star.textureID);
-                    } else {
-                        if (ett.hasComponent<rtc::Speed>()) {
-
-                            sprite.setPosition(
-                                sprite.getPosition().x - (ett.getComponent<rtc::Speed>().speed * timeSinceLastFrame),
-                                sprite.getPosition().y);
-                        }
-                    }
-                });
-            }
-
-        private:
-            sf::RenderWindow &_win;
-            EntityManager &_ettMgr;
-            sfutils::ResourceManager<sf::Texture, SpriteT> &_textures;
-            gutils::EventManager &_evtMgr;
-            logging::Logger _log{"DemoAnimationSystem", logging::Debug};
-
-            std::random_device _rd;
-            std::mt19937 _mt{_rd()};
-            std::uniform_real_distribution<float> _distX{0.f, 1920.f};
-            std::uniform_real_distribution<float> _distY{0.f, 1080.f};
-            std::uniform_real_distribution<float> _speedDistribution{10.f, 100.f};
-        };
-
         class AnimationSystem
         {
         public:
@@ -214,22 +79,6 @@ namespace rtype
                 }
             }
 
-            void draw() noexcept
-            {
-                _ettMgr.for_each<rtc::Animation>([this](rtype::Entity &ett) {
-                    if (this->_debugMode) {
-                        _win.draw(ett.getComponent<rtc::BoundingBox>().shapeDebug);
-                        try {
-                            _win.draw(_quadTree.getNode(ett.getID())->_shapeDebug);
-                        }
-                        catch (const std::out_of_range &error) {
-                            _log(lg::Error) << error.what() << std::endl;
-                        }
-                    }
-                    _win.draw(ett.getComponent<rtc::Animation>().anim);
-                });
-            }
-
             void loadAnimation(const Sprite &sprite, const Animation &anim,
                                [[maybe_unused]] sf::Vector2<unsigned int> &&sprInfo,
                                sf::Vector3<unsigned int> &&sheetInfo)
@@ -244,8 +93,8 @@ namespace rtype
 
             void changePlayerAnim() noexcept
             {
-                auto[movement, animation, player] = _ettMgr[_playerID].getComponents<rtc::Movement, rtc::Animation,
-                    rtc::Player>();
+                auto[movement, player] = _ettMgr[_playerID].getComponents<rtc::Movement, rtc::Player>();
+                auto& animation = _ettMgr[_playerID].getComponent<rtc::Animation>();
                 rtc::Direction dir = movement.dir;
                 auto curAnim = static_cast<AnimT>(animation.currentAnim);
                 size_t currentFrame = animation.anim.getCurrentFrame();
@@ -319,7 +168,6 @@ namespace rtype
 
         ~DemoScene() noexcept = default;
 
-    public:
         void enter() noexcept final;
         void resume() noexcept final;
         void pause() noexcept final;
@@ -336,7 +184,6 @@ namespace rtype
         using UIGUI = BaseGUI<cfg::play::nbWidgets>;
         using AnimDemo = demo::AnimationSystem::Animation::EnumType;
 
-    private:
         void __configure() noexcept;
         bool __setGUI() noexcept;
         void __parseConfig(const std::string &faction);
@@ -345,10 +192,12 @@ namespace rtype
         void __createGameObjects() noexcept;
         void __bulletSystem(double timeSinceLastFrame) noexcept;
         void __loadBulletSprite() noexcept;
-        void __loadFlareSprite() noexcept;
+        void __loadFogSprite() noexcept;
+        void __loadStarSprite() noexcept;
+        void __loadPlanetSprite() noexcept;
         void __setPlayerCallbacks() noexcept;
         void __unbindPlayerCallbacks() noexcept;
-    private:
+
         UIGUI _gui;
         sfutils::ResourceManager<sf::Texture, demo::SpriteT> _textures;
         std::unordered_map<std::string, sf::IntRect> _boundingBoxFactions;
@@ -356,7 +205,7 @@ namespace rtype
         EntityManager _ettMgr;
         QuadTree<EntityManager> _quadTree{sf::FloatRect(0.f, 0.f, 1920, 1080), _ettMgr};
         demo::AnimationSystem _animSystem{_win, _ettMgr, _debugMode, _textures, _quadTree};
-        demo::StarfieldSystem _starfieldSystem{_win, _ettMgr, _textures, _evtMgr};
+        demo::field::FieldSystem _fieldSystem{_ettMgr, _textures, _evtMgr};
         std::chrono::time_point<std::chrono::steady_clock> _lastShoot;
         Entity::ID _playerID;
     };
