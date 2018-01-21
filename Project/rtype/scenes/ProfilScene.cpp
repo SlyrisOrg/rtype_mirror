702170295237a0f4e7f228091009cf0f02b89438
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2018-01-21 15:30:40 +0100

    Add netproto + protocol, and choose game Mode in profilScene
    Change-Id: Icebd66853004d53c15dd998e5ac96897952e6267

--- Project/rtype/scenes/ProfilScene.cpp
+++ Project/rtype/scenes/ProfilScene.cpp
@@ -7,6 +7,7 @@
 #include <rtype/scenes/ProfilScene.hpp>
 #include <rtype/api/rtype-api.hpp>
 #include <rtype/config/SpaceshipConfig.hpp>
+#include <rtype/config/PlayerConfig.hpp>
 
 namespace rtype
 {
@@ -48,14 +49,23 @@ namespace rtype
         _gui[ProfilWidgets::MainMenu].enable();
     }
 
+    void ProfilScene::__closeModeWindow() noexcept
+    {
+        _evtMgr.emit<gutils::evt::PlaySoundEffect>(Configuration::SoundEffect::ClickSmooth);
+        _gui[ProfilWidgets::ModeWindow].hide();
+        _gui[ProfilWidgets::PlayButton].enable();
+    }
+
     //! Private member functions
     void ProfilScene::__subscribeEvents() noexcept
     {
         using pb = CEGUI::PushButton;
         using evt = CEGUI::Event;
+        using tg = CEGUI::ToggleButton;
         using ps = ProfilScene;
 
         _gui[ProfilWidgets::BackButton].subscribeEvent(pb::EventClicked, evt::Subscriber(&ps::__resume, this));
+        _gui[ProfilWidgets::ModeOkButton].subscribeEvent(pb::EventClicked, evt::Subscriber(&ps::__goingToQueue, this));
         _gui[ProfilWidgets::ExitDesktopButton].subscribeEvent(pb::EventClicked,
                                                               evt::Subscriber(&ps::__leaveGame, this));
         _gui[ProfilWidgets::LogOutButton].subscribeEvent(pb::EventClicked, evt::Subscriber(&ps::__logOut, this));
@@ -63,9 +73,19 @@ namespace rtype
                                                           evt::Subscriber(&ps::__optionsWindow, this));
         _gui[ProfilWidgets::OptionsCloseButton].subscribeEvent(pb::EventClicked,
                                                                evt::Subscriber(&ps::__closeOptionsWindow, this));
-        //TODO Temporary going to QScene, Need to offer game choice before.
+
+        _gui[ProfilWidgets::ModeOptionsCloseButton].subscribeEvent(pb::EventClicked,
+                                                                   evt::Subscriber(&ps::__closeModeWindow, this));
         _gui[ProfilWidgets::PlayButton].subscribeEvent(pb::EventClicked,
-                                                       evt::Subscriber(&ps::__goingToQueue, this));
+                                                       evt::Subscriber(&ps::__modeWindow, this));
+        _gui[ProfilWidgets::ModeLocalCheck].subscribeEvent(tg::EventMouseClick,
+                                                           evt::Subscriber(&ps::__checkModeLocal, this));
+        _gui[ProfilWidgets::ModeTwoCheck].subscribeEvent(tg::EventMouseClick,
+                                                         evt::Subscriber(&ps::__checkModeTwo, this));
+        _gui[ProfilWidgets::ModeThreeCheck].subscribeEvent(tg::EventMouseClick,
+                                                           evt::Subscriber(&ps::__checkModeThree, this));
+        _gui[ProfilWidgets::ModeFourCheck].subscribeEvent(tg::EventMouseClick,
+                                                          evt::Subscriber(&ps::__checkModeFour, this));
     }
 
     bool ProfilScene::__setGUI() noexcept
@@ -86,8 +106,63 @@ namespace rtype
 
     void ProfilScene::__goingToQueue() noexcept
     {
+        _gui[ProfilWidgets::ModeWindow].hide();
         _evtMgr.emit<gutils::evt::StopMusic>(Configuration::Music::MarketInstrumentalAmb);
-        _evtMgr.emit<gutils::evt::ChangeScene>(Scene::Queue);
+        if (cfg::player::mode == matchmaking::Mode::Solo) {
+            _evtMgr.emit<gutils::evt::ChangeScene>(Scene::Demo);
+        } else {
+            _evtMgr.emit<gutils::evt::ChangeScene>(Scene::Queue);
+        }
+    }
+
+    void ProfilScene::__checkModeFour() noexcept
+    {
+        auto &modeFourCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeFourCheck]);
+        auto &modeTwoCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeTwoCheck]);
+        auto &modeLocalCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeLocalCheck]);
+        auto &modeThreeCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeThreeCheck]);
+        cfg::player::mode = matchmaking::Mode::Quatuor;
+        __internalDisableToggleButton(modeFourCheck, modeTwoCheck, modeLocalCheck, modeThreeCheck);
+        _log(lg::Debug) << "The game mode is set to \"Four players\"" << std::endl;
+    }
+
+    void ProfilScene::__checkModeThree() noexcept
+    {
+        auto &modeThreeCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeThreeCheck]);
+        auto &modeTwoCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeTwoCheck]);
+        auto &modeLocalCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeLocalCheck]);
+        auto &modeFourCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeFourCheck]);
+        cfg::player::mode = matchmaking::Mode::Trio;
+        __internalDisableToggleButton(modeThreeCheck, modeLocalCheck, modeTwoCheck, modeFourCheck);
+        _log(lg::Debug) << "The game mode is set to \"Three players\"" << std::endl;
+    }
+
+    void ProfilScene::__checkModeTwo() noexcept
+    {
+        auto &modeTwoCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeTwoCheck]);
+        auto &modeLocalCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeLocalCheck]);
+        auto &modeThreeCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeThreeCheck]);
+        auto &modeFourCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeFourCheck]);
+        cfg::player::mode = matchmaking::Mode::Duo;
+        __internalDisableToggleButton(modeTwoCheck, modeLocalCheck, modeThreeCheck, modeFourCheck);
+        _log(lg::Debug) << "The game mode is set to \"Two players\"" << std::endl;
+    }
+
+    void ProfilScene::__checkModeLocal() noexcept
+    {
+        auto &modeLocalCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeLocalCheck]);
+        auto &modeTwoCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeTwoCheck]);
+        auto &modeThreeCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeThreeCheck]);
+        auto &modeFourCheck = static_cast<CEGUI::ToggleButton &>(_gui[ProfilWidgets::ModeFourCheck]);
+        cfg::player::mode = matchmaking::Mode::Solo;
+        __internalDisableToggleButton(modeLocalCheck, modeTwoCheck, modeThreeCheck, modeFourCheck);
+        _log(lg::Debug) << "The game mode is set to \"Local\"" << std::endl;
+    }
+
+    void ProfilScene::__modeWindow() noexcept
+    {
+        _gui[ProfilWidgets::ModeWindow].setVisible(true);
+        _gui[ProfilWidgets::PlayButton].disable();
     }
 
     void ProfilScene::__getData() const noexcept
@@ -113,9 +188,12 @@ namespace rtype
         _anims = cfg::ship::multiAnimMap[cfg::profil::Faction::toString(cfg::profil::faction)];
         _anims.curAnim.setPosition(static_cast<float>(cfg::game::width / 2 - 337),
                                    static_cast<float>(cfg::game::height / 2 - 237));
+        _gui[ProfilWidgets::ModeOptionsHead].setPosition(_gui[ProfilWidgets::ModeOptionsHead].getPosition());
+        _gui[ProfilWidgets::ModeOptionsRealHead].setPosition(_gui[ProfilWidgets::ModeOptionsRealHead].getPosition());
         _gui[ProfilWidgets::MainMenuHead].setPosition(CEGUI::UVector2(CEGUI::UDim(0, 50), CEGUI::UDim(0, -24)));
         _gui[ProfilWidgets::OptionsHead].setPosition(CEGUI::UVector2(CEGUI::UDim(0, 4), CEGUI::UDim(0, -57)));
         _gui[ProfilWidgets::OptionsRealHead].setPosition(CEGUI::UVector2(CEGUI::UDim(0, 309), CEGUI::UDim(0, -15)));
+        cfg::player::mode = matchmaking::Mode::Solo;
     }
 
     void ProfilScene::__configure() noexcept
@@ -135,17 +213,22 @@ namespace rtype
     void ProfilScene::__cancelMainMenuWindow() noexcept
     {
         _gui[ProfilWidgets::MainMenu].setVisible(false);
+        _gui[ProfilWidgets::PlayButton].enable();
     }
 
     void ProfilScene::__mainMenuWindow() noexcept
     {
         _gui[ProfilWidgets::MainMenu].setVisible(true);
+        _gui[ProfilWidgets::PlayButton].disable();
     }
 
     void ProfilScene::__checkResume(const sf::Keyboard::Key &key) noexcept
     {
         //TODO: We need to do more checks when we will have shop and other window
-        if (key == sf::Keyboard::Escape && !_pause && !_gui[ProfilWidgets::MainMenu].isDisabled()) {
+        if (key == sf::Keyboard::Escape && _gui[ProfilWidgets::ModeWindow].isVisible()) {
+            _gui[ProfilWidgets::ModeWindow].hide();
+            _gui[ProfilWidgets::PlayButton].enable();
+        } else if (key == sf::Keyboard::Escape && !_pause && !_gui[ProfilWidgets::MainMenu].isDisabled()) {
             pause();
         } else if (key == sf::Keyboard::Escape && _pause && !_gui[ProfilWidgets::MainMenu].isDisabled()) {
             resume();
